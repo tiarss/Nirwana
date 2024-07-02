b97544a8025b82e5d82f7cd947546cb90111750e
bahtiarsubrata <subratabahtiar@gmail.com>
2024-07-02 15:42:24 +0700

    feat: create border and add padding

--- Nirwana/ContentView.swift
+++ Nirwana/ContentView.swift
@@ -1,21 +1,18 @@
-//
-//  ContentView.swift
-//  Nirwana
-//
-//  Created by Bahtiar Subrata on 02/07/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+            })
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
-        .padding()
+        .padding(100)
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 10)
     }
 }
 
