21370b479dbe2a7893e84b7eddc6dad0ebec0327
bahtiarsubrata <subratabahtiar@gmail.com>
2024-07-02 15:29:59 +0700

    Initial Commit

---
+++ Nirwana/NirwanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NirwanaApp.swift
+//  Nirwana
+//
+//  Created by Bahtiar Subrata on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct NirwanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
